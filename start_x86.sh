cda0602c9b555054e6814da08897b6aeaa643d3d
JockDaRock <jock.da.rock@gmail.com>
2019-07-29 16:35:50 -0500

    test123

--- start_x86.sh
+++ start_x86.sh
@@ -11,11 +11,12 @@ fi
 
 # start desktop manager
 echo "STARTING X"
-# startx
+
+startx
 
 # uncomment to start x without mouse cursor
 # startx -- -nocursor
 
 # uncomment to open an application instead of the desktop
 # startx xterm
-startx code
+# startx code
